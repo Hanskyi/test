4bf6a13a8b6a7c5fda3cb696a0068807a17d48b0
hanskyi <han.eginbaev@gmail.com>
2023-12-29 17:29:19 +0600

    created a cell component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import EllItem from "./Components/СellItem";
 
 function App() {
   return (
-    <div className="App">
-
+    <div className="App" style={{margin: "50px auto", maxWidth: "1200"}}>
+      <EllItem/>
     </div>
   );
 }
---
+++ src/Components/СellItem.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './сellItemStyle.css';
+
+const EllItem = () => {
+    return (
+        <div className="сell-item">
+            
+        </div>
+    );
+};
+
+export default EllItem;
